b2a7569d9c5936e1976f2968d645e9f5b678bf7e
Kartikey Sharma <kartikeysharma2004@gmail.com>
2026-09-13 13:38:46 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minValue = INT_MAX;
+        int maxProfit = -1;
+        for(int i=0; i<prices.size(); i++)
+        {
+            minValue = min(minValue , prices[i]);
+            maxProfit = max(maxProfit , prices[i] - minValue);
+        }
+        return maxProfit;
+    }
+};
